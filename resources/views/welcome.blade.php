419069b872e88876c92f1dc2ce9ffca4f625c318
Baddooer <baddur29@gmail.com>
2024-01-09 09:20:08 +0700

    uda atur css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,43 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+
+
+    <!--start-->
+    <title>Zayn Wedding - Wedding Organizer</title>
+    <meta name="title" content="Zayn Wedding - Wedding Organizer">
+    <meta name="description" content="Wedding Organizer di Jakarta Barat">
+
+    <!--favicon-->
+    <link rel="shortcut icon" href="./favicon.svg" type="image/svg+xml">
+
+    <!--font-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&family=Forum&
+    display=swap" rel="stylesheet">
+
+    <!--custom css link-->
+    <link rel="stylesheet" href="/resources/css/style.css">
+
+    <!--preload images-->
+    <link rel="preload" as="image" href="./resources/images/hero-slider-1.jpg">
+    <link rel="preload" as="image" href="./resources/images/hero-slider-2.jpg">
+    <link rel="preload" as="image" href="./resources/images/hero-slider-3.jpg">
 </head>
+
 <body>
-    <h1>halooo alvin 2</h1>
+
+
+
+<!--custom js link-->
+<script src="./resources/js/script.js"></script>
+
+<!--ionicon link-->
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+
 
 </body>
 </html>
